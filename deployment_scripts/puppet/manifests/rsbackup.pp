ade595c7e25fe7fe4960b4fac8a31df9cf552328
Stefan Marx <smarx@mirantis.com>
2016-04-14 10:03:39 +0200

    added firewall rule

--- deployment_scripts/puppet/manifests/rsbackup.pp
+++ deployment_scripts/puppet/manifests/rsbackup.pp
@@ -3,3 +3,12 @@ $rsbackup_hash    = hiera('rsync-backup_role',{})
 class {'rsbackup':
   base => $rsbackup_hash['base']
 }
+
+class {'::firewall':}
+
+firewall {'837 rsync':
+  port   => 837,
+  proto  => 'tcp',
+  action => 'accept',
+}
+
